9637943d2873a8b2ba6a2893068f49af54be5d9d
marcos <marcosvila96@gmail.com>
2023-08-11 11:40:29 -0300

    vercel deploy

--- src/db.js
+++ src/db.js
@@ -4,5 +4,6 @@ const {db} = require('./config.js')
 const pool = new Pool({
     connectionString: process.env.POSTGRES_URL + "?sslmode=require",
   })
+console.log('conection success')
 
 module.exports = pool
